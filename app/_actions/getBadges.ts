3e05300730c92764ef3edc3aed43c7b55747e78b
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-08-27 14:13:26 +0300

    Badge list fetch action has been written

---
+++ app/_actions/getBadges.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { IBadge } from "@/_types";
+
+export default async function getBadges(): Promise<IBadge[]> {get
+    try {
+        const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+        const url = `${baseUrl}/api/badge/list`
+        const { data } = await axios.get<IBadge[]>(url);
+        return data;
+    } catch (e) {
+        return []
+    }
+}
