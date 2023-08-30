20dbae5b836f3966cdd858b2b711b981a0149b37
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-08-30 18:56:08 +0300

    context menu has been created

---
+++ app/_components/game/CellContextMenu.tsx
@@ -0,0 +1,36 @@
+import { LuDelete } from "react-icons/lu";
+
+interface Props {
+    dimension: number,
+    onMouseLeave: () => void,
+    onSelectNumber: (no: number) => void,
+    onDeleteNumber: () => void,
+    style: {
+        [key: string]: string
+    }
+}
+
+const CellContextMenu = ({ dimension, style, onMouseLeave, onSelectNumber, onDeleteNumber }: Props) => {
+    const numbers = Array.from({ length: dimension });
+    return (
+        <div
+            onMouseLeave={onMouseLeave}
+            style={style}
+            className="absolute grid grid-cols-3 w-40 md:w-44 lg:w-48 z-10 overflow-hidden shadow-[0px_0px_5px_0px_#7e22ce] rounded-lg bg-gray-700">
+
+            {numbers.map((_, index) => (
+                <div key={index}
+                     onClick={() => onSelectNumber(index + 1)}
+                     className="cursor-pointer rounded grid place-items-center h-12 text-white hover:bg-gray-600 text-base lg:text-lg xl:text-xl">{index + 1}</div>
+            ))}
+
+            <div className="col-span-3">
+                <LuDelete
+                    onClick={onDeleteNumber}
+                    className="cursor-pointer ml-auto p-2 text-5xl text-red-500 hover:text-red-600"/>
+            </div>
+        </div>
+    );
+};
+
+export default CellContextMenu;
