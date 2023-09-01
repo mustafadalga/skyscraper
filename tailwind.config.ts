3f4047f536cfeabcd87ecec1c83c24e9f31579bb
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-09-01 21:52:41 +0300

    Tailwind animation has been configured

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,41 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            //resource : https://animista.net/
+            animation: {
+                "scale-out-center": "scale-out-center 0.25s cubic-bezier(0.550, 0.085, 0.680, 0.530) forwards",
+                "scale-in-center": "scale-in-center 0.25s cubic-bezier(0.250, 0.460, 0.450, 0.940) forwards",
+            },
+            keyframes: {
+                "scale-out-center": {
+                    "0%": {
+                        opacity: "1",
+                        transform: "scale(1)"
+                    },
+                    "100%": {
+                        opacity: "0",
+                        transform: "scale(0)"
+                    }
+                },
+                "scale-in-center": {
+                    "0%": {
+                        opacity: "0",
+                        transform: "scale(0)"
+                    },
+                    "100%": {
+                        opacity: "1",
+                        transform: "scale(1)"
+                    }
+                },
+            }
+        }
+    }
 }
 export default config
