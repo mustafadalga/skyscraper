08f22082faca71b8c920cf98297cc365e7211fe8
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-09-01 21:30:04 +0300

    Project enums have been defined

---
+++ app/_enums/index.ts
@@ -0,0 +1,31 @@
+export enum Badge {
+    NoviceBuilder = "novice_builder",
+    FastBuilder = "fast_builder",
+    Perfectionist = "perfectionist",
+    SkyscraperEnthusiast = "skyscraper_enthusiast",
+    NoHintsNeeded = "no_hints_needed",
+    TimeLord = "time_lord",
+    PuzzleExplorer = "puzzle_explorer",
+    MasterPlanner = "master_planner",
+    TheVisionary = "the_visionary",
+    TheHighScorer = "the_high_scorer",
+    SeasonedBuilder = "seasoned_builder",
+    PuzzleMaestro = "puzzle_maestro",
+    PuzzleVirtuoso = "puzzle_virtuoso",
+    PuzzleLegend = "puzzle_legend",
+    PuzzleOlympian = "puzzle_olympian",
+    PuzzleImmortal = "puzzle_immortal"
+}
+
+export enum Difficulty {
+    EASY = "easy",
+    MEDIUM = "medium",
+    HARD = "hard",
+}
+
+export enum Direction {
+    TOP = "top",
+    LEFT = "left",
+    RIGHT = "right",
+    BOTTOM = "bottom"
+}
