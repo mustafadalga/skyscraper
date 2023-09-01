deeee00734212a74a88a45e053dbe3f969ced66d
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-09-01 21:30:55 +0300

    grid class names have been defined

--- app/_constants/index.ts
+++ app/_constants/index.ts
@@ -125,3 +125,54 @@ export const badgeLevelDetails: BadgeLevelDetails = {
         "dimension": 12
     }
 }
+
+export const tailwindGridClassNames = [
+    {
+        "row": "",
+        "col": ""
+    },
+    {
+        "row": "",
+        "col": ""
+    },
+    {
+        "row": "grid-rows-3",
+        "col": "grid-cols-3"
+    },
+    {
+        "row": "grid-rows-4",
+        "col": "grid-cols-4"
+    },
+    {
+        "row": "grid-rows-5",
+        "col": "grid-cols-5"
+    },
+    {
+        "row": "grid-rows-6",
+        "col": "grid-cols-6"
+    },
+    {
+        "row": "grid-rows-7",
+        "col": "grid-cols-7"
+    },
+    {
+        "row": "grid-rows-8",
+        "col": "grid-cols-8"
+    },
+    {
+        "row": "grid-rows-9",
+        "col": "grid-cols-9"
+    },
+    {
+        "row": "grid-rows-10",
+        "col": "grid-cols-10"
+    },
+    {
+        "row": "grid-rows-11",
+        "col": "grid-cols-11"
+    },
+    {
+        "row": "grid-rows-12",
+        "col": "grid-cols-12"
+    }
+]
