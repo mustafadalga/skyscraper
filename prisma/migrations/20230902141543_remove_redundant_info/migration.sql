d006f134878538e594cce0b5f15d94d4d5f0f5b1
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-09-02 17:15:56 +0300

    timeTaken has been removed

---
+++ prisma/migrations/20230902141543_remove_redundant_info/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `timeTaken` on the `Game` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Game" DROP COLUMN "timeTaken";
