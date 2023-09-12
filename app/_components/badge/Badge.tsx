ba0b541cbbc202f6ededce4ebf3e7b829d5d9016
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-09-12 22:29:09 +0300

    Badge component has been designed

---
+++ app/_components/badge/Badge.tsx
@@ -0,0 +1,15 @@
+import BadgeIcon from "./BadgeIcon";
+
+interface Props {
+    id: string,
+    name: string
+}
+
+export default function Badge({ id, name }: Props) {
+    return (
+        <div className="grid place-items-center gap-2.5">
+            <BadgeIcon badgeID={id} className="text-2xl lg:text-3xl xl:text-4xl text-purple-500"/>
+            <h5 className="text-center mt-auto text-sm md:text-base xl:text-xl font-medium text-gray-500">{name}</h5>
+        </div>
+    );
+};
