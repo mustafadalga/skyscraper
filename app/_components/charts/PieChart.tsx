bd222a76d845f387c5a6b3c897cc89c4a2d935ad
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-09-13 09:17:12 +0300

    Pie chart component has been developed

---
+++ app/_components/charts/PieChart.tsx
@@ -0,0 +1,45 @@
+"use client";
+import Chart from 'chart.js/auto';
+import { useRef, useEffect } from "react";
+
+interface IDataSet {
+    label: string,
+    data: number[],
+    backgroundColor: string[]
+}
+
+interface Props {
+    data: {
+        labels: string[],
+        datasets: IDataSet[]
+    },
+    className: string
+}
+
+export default function PieChart({ data, className }: Props) {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const chartRef = useRef<Chart<'pie', number[], string> | null>(null);
+
+    useEffect(() => {
+        if (canvasRef && canvasRef.current) {
+            chartRef.current = new Chart(canvasRef.current, {
+                type: "pie",
+                data,
+            });
+        }
+
+        return () => {
+            if (chartRef.current) {
+                chartRef.current.destroy();
+                chartRef.current = null;
+            }
+        };
+    }, [ data ]);
+
+    return (
+        <div className={className}>
+            <canvas ref={canvasRef}/>
+        </div>
+
+    );
+}
