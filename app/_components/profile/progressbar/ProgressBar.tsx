36441e320424c145e081e3acaf0b3a3056579fe4
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-09-17 00:32:39 +0300

    Animation has been added

--- app/_components/profile/progressbar/ProgressBar.tsx
+++ app/_components/profile/progressbar/ProgressBar.tsx
@@ -17,18 +17,21 @@ const IconChevron = () => {
 export default function ProgressBar({ badge }: Props) {
     const progressBarStyle = {
         '--progress-bar-width': `${badge.percent}%`,
-        "--percent": `${badge.percent - 2}%`
+        "--percent": `${badge.percent - 5}%`,
+        "--badge-icon-color": `${badge.percent == 100 ? "#ffffff" : "#a855f7"}`,
+        "--percent-margin-left": `${badge.percent > 98 ? badge.percent - 8 : badge.percent - 5}%`
 
     } as CSSProperties;
+
     return (
         <div
             style={progressBarStyle}
             className={`${progressBar.progressBar} group flex items-center relative w-full bg-gray-200 h-6 rounded-lg after:absolute after:h-full after:bg-gradient-to-r after:from-purple-200 after:to-purple-700 after:rounded-lg`}>
 
             <span
-                className={`${progressBar.percent} absolute z-20 ml-72 mt-1 text-xs text-white`}>%{Math.round(badge.percent)}</span>
+                className={`${progressBar.percent} absolute z-20 top-1/2 -translate-y-1/2 text-xs text-white`}>%{Math.round(badge.percent)}</span>
             <BadgeIcon badgeID={badge.id}
-                       className={`${progressBar.badgeIconColor} group absolute z-10 right-1.5 mt-0.5 text-sm`}/>
+                       className={`${progressBar.badgeIconColor} group absolute z-10 right-1.5 top-1/2 -translate-y-1/2 text-sm`}/>
             <div
                 className="opacity-0 invisible group-hover:opacity-100 group-hover:visible transition absolute bottom-full h-3 w-full">
                 <div
