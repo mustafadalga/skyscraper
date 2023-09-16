e281e04bf56e2858dad703110ed36c01311235d8
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-09-17 00:06:08 +0300

    Empty state component has been designed

---
+++ app/_components/hall-of-wins/EmptyGameList.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function EmptyGameList() {
+    return (
+        <div className="my-20 text-center grid gap-1 justify-center">
+            <h2 className="text-xl lg:text-2xl text-purple-500 font-semibold mb-4">There is no game yet!</h2>
+            <Link href="/game"
+                  className="mx-auto w-60 bg-purple-500 text-white px-4 py-2 rounded text-sm lg:text-base rounded-md hover:bg-purple-600 transition">Start
+                a New Game</Link>
+        </div>
+    );
+};
