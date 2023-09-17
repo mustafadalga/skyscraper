5dd100210dba9e500097307f3543c1292bcc6ec7
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-09-17 16:00:31 +0300

    Modal store has been structured

---
+++ app/_store/useModalGame.ts
@@ -0,0 +1,38 @@
+import { create } from 'zustand';
+import { Game } from ".prisma/client";
+import axios from "axios";
+import { toast } from "react-toastify";
+import handleAxiosError from "@/_utilities/handleAxiosError";
+import useHallOfWins from "@/_components/hall-of-wins/useHallOfWins";
+
+interface ModalGameStore {
+    isOpen: boolean;
+    game: Game | null,
+    onOpen: () => void;
+    onClose: () => void;
+    setGame: (id: string) => Promise<void>
+}
+
+export default create<ModalGameStore>((set) => ({
+    isOpen: false,
+    game: null,
+    onOpen: () => set({ isOpen: true }),
+    onClose: () => set({ isOpen: false }),
+    setGame: async (id: string) => {
+        try {
+            const userID = useHallOfWins.getState().user?.id;
+            const url = `/api/game/${id}`;
+            const { data: game } = await axios.get(url, {
+                params: {
+                    userID
+                }
+            });
+            set({ game })
+        } catch (error) {
+            const { message } = handleAxiosError(error, "Oops! Something went wrong while loading the game details. Please try again later.");
+            toast.error(message)
+        }
+    }
+}))
+
+
