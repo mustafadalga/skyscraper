e63407daf45b59a06ffecd535d57946976451a4f
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-09-19 21:35:46 +0300

    Title has been responsive

--- app/(app)/_components/hall-of-wins/Header.tsx
+++ app/(app)/_components/hall-of-wins/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className="grid place-items-center gap-5 mb-6">
             <FaCrown className="text-orange-400 text-5xl lg:text-6xl xl:text-7xl mb-2"/>
-            <h1 className="text-4xl font-semibold text-purple-600">Hall Of Wins</h1>
+            <h1 className="text-3xl lg:text-4xl font-semibold text-purple-600">Hall Of Wins</h1>
         </div>
     );
 };
--- app/(app)/_components/leader-board/Header.tsx
+++ app/(app)/_components/leader-board/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className="grid place-items-center gap-5 mb-6">
             <GiLaurelsTrophy className="text-orange-400 text-5xl lg:text-6xl xl:text-7xl mb-2"/>
-            <h1 className="text-4xl font-semibold text-purple-600">Leader Board</h1>
+            <h1 className="text-3xl lg:text-4xl font-semibold text-purple-600">Leader Board</h1>
         </div>
     );
 };
